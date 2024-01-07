4db0107af6256cc0556b2be7e1e3418906cabb4f
LosAngeles971 <LosAngeles1791@gmail.com>
2024-01-07 23:46:28 +0100

    ui refactoring

--- business/ui/page_messages.go
+++ business/ui/page_messages.go
@@ -1,6 +1,9 @@
 package ui
 
-import "github.com/rivo/tview"
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
 
 func (a *CBAToolApp) callQuit() {
 	modal := tview.NewModal().SetText("Do you want to quit the application?").AddButtons([]string{"Quit", "Cancel"}).SetDoneFunc(func(buttonIndex int, buttonLabel string) {
@@ -22,4 +25,14 @@ func (a *CBAToolApp) callErrorMessage(e error) {
 		a.callMenuPage()
 	})
 	a.app.SetRoot(modal, true)
+}
+
+func (a *CBAToolApp) callLineInput(message string) {
+	inputField := tview.NewInputField()
+	inputField.SetLabel(message)
+	inputField.SetFieldWidth(10)
+	inputField.SetAcceptanceFunc(tview.InputFieldInteger)
+	inputField.SetDoneFunc(func(key tcell.Key) {
+		a.callMenuPage()
+	})
 }
